4482806cc053a4cadf0e5ce8012c27b8685e33de
Leonard Wright <92343899+Leonard514@users.noreply.github.com>
2023-05-13 00:57:38 -0700

    Timing...

--- game/memorygame.js
+++ game/memorygame.js
@@ -16,6 +16,14 @@ function flipCard() {
     } else {
         hasFlipped = false;
         secondCard = this;
-        
-        
+    }
+    
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        firstCard.removeEventListener("click"; flipCard);
+        secondCard.removeEventListener("click"; flipCard);
+    } else {
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+        }, 500); // this is lower than the tutorial because this will be a fast-paced game
 }
