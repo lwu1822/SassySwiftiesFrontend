398a3578f97ae4bb5ad17fbe6e1539748cbb9d31
Leonard Wright <92343899+Leonard514@users.noreply.github.com>
2023-05-12 23:45:37 -0700

    Cleanup

--- game/memorygame.js
+++ game/memorygame.js
@@ -1,56 +1,58 @@
 const cards = document.queryselectorAll('.box');
 
+cards.forEach(card => card.addEventListener('click', flipCard));
+
 function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
+    //if (lockBoard) return;
+    //if (this === firstCard) return;
   
     this.classList.toggle('flip');
   
-    if (!hasFlippedCard) {
-      hasFlippedCard = true;
-      firstCard = this;
+    //if (!hasFlippedCard) {
+      //hasFlippedCard = true;
+      //firstCard = this;
   
-      return;
+      //return;
     }
   
-    secondCard = this;
-    checkForMatch();
+    //secondCard = this;
+    //checkForMatch();
   }
   
-function checkForMatch() {
-    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+//function checkForMatch() {
+    //let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
   
-    isMatch ? disableCards() : unflipCards();
+    //isMatch ? disableCards() : unflipCards();
   }
   
-function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
+//function disableCards() {
+    //firstCard.removeEventListener('click', flipCard);
+    //secondCard.removeEventListener('click', flipCard);
   
-    resetBoard();
+    //resetBoard();
   }
   
-function unflipCards() {
-    lockBoard = true;
+//function unflipCards() {
+    //lockBoard = true;
   
-    setTimeout(() => {
-      firstCard.classList.remove('flip');
-      secondCard.classList.remove('flip');
+    //setTimeout(() => {
+      //firstCard.classList.remove('flip');
+      //secondCard.classList.remove('flip');
   
-      resetBoard();
-    }, 1500);
+      //resetBoard();
+    //}, 1500);
   }
   
-function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
+//function resetBoard() {
+    //[hasFlippedCard, lockBoard] = [false, false];
+    //[firstCard, secondCard] = [null, null];
   }
   
-function shuffle() {
-    cards.forEach(card => {
-      let randomPos = Math.floor(Math.random() * 12);
-      card.style.order = randomPos;
+//function shuffle() {
+    //cards.forEach(card => {
+      //let randomPos = Math.floor(Math.random() * 12);
+      //card.style.order = randomPos;
     });
   }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+
