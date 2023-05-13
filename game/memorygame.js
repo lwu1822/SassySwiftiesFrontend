bf63d0d66f3dcc1b7c395f3ded7f331ba0605390
Leonard Wright <92343899+Leonard514@users.noreply.github.com>
2023-05-13 00:45:45 -0700

    Update memorygame.js

--- game/memorygame.js
+++ game/memorygame.js
@@ -2,10 +2,20 @@ const cards = document.querySelectorAll('.box');
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
+let hasFlipped = false;
+let firstCard, secondCard;
+
 function flipCard() {
     console.log("clickity time!")
   
     this.classList.toggle('flip');
   
-  
+    if (!hasFlipped) {
+        hasFlipped = true;
+        firstCard = this;
+    } else {
+        hasFlipped = false;
+        secondCard = this;
+        
+        
 }
