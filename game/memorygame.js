37454b79e45df6345a7fd0714bba6e42a6094680
Leonard Wright <92343899+Leonard514@users.noreply.github.com>
2023-05-18 21:08:57 -0700

    Cleanup

--- game/memorygame.js
+++ game/memorygame.js
@@ -13,7 +13,7 @@ var sec = 0;
 var money = 0;
 var beforeTime = 0;
 var afterTime = 0;
-console.log(sec)
+// console.log(sec)
 let gameInactive = true;
 let gameOver = false;
 let hasFlipped = false;
@@ -34,7 +34,7 @@ let firstCard, secondCard;
 // match before starting
 
 function detectCheating() {
-    console.log("Checked for cheating")
+    // console.log("Checked for cheating")
     beforeTime = new Date().getTime();
     debugger;
     afterTime = new Date().getTime();
@@ -52,7 +52,7 @@ function clearVar() {
 }
 
 function noFlip() {
-    console.log("event listener removed!");
+    // console.log("event listener removed!");
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
     clearVar();
@@ -60,17 +60,17 @@ function noFlip() {
 
 function reject() {
     lockdown = true;
-    console.log("failure to match detected! Locking board...");        
+    // console.log("failure to match detected! Locking board...");        
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
         clearVar();
-        console.log("board unlocked");
+        // console.log("board unlocked");
     }, 500); // this is lower than the tutorial because this will be a fast-paced game
 }
 
 function updateMoney() {
-    console.log("Timecheck: " +sec);
+    // console.log("Timecheck: " +sec);
     if (sec < 10) {
         money += 3;
     } else if (sec < 20) {
@@ -78,12 +78,12 @@ function updateMoney() {
     } else {
         money += 1
     }
-    console.log(money);
+    // console.log(money);
     document.getElementById("swifties").innerHTML = "Swifties Earned: " + money + " Swifties";
 }
 
 function timer() {
-    console.log(sec);
+    // console.log(sec);
     if (gameInactive) return;
     if (sec == 29) {
         document.getElementById("time").innerHTML = "Time's up!";
@@ -98,6 +98,7 @@ function timer() {
 }
 
 function timedExecutables() {
+    if (gameOver) return;
     detectCheating(); 
     timer();
 }
@@ -112,7 +113,7 @@ function checkMatching() {
      if (firstCard.dataset.framework === secondCard.dataset.framework) {
             noFlip();
             updateMoney();
-            console.log("match detected!");
+            // console.log("match detected!");
         } else {
             reject()
         }
@@ -122,10 +123,10 @@ function flipCard() {
     if (lockdown) return;
     if (gameOver) return;
     if (this === firstCard) {
-        console.log("Select another card");
+        // console.log("Select another card");
         return;
     }
-    console.log("clickity time!");
+    // console.log("clickity time!");
   
     this.classList.add('flip');
   
@@ -136,12 +137,12 @@ function flipCard() {
         }
         hasFlipped = true;
         firstCard = this;
-        console.log("first card detected! This card is ");
-        console.log(firstCard);
+        // console.log("first card detected! This card is ");
+        // console.log(firstCard);
     } else {
         secondCard = this;
-        console.log("second card detected! This card is");
-        console.log(secondCard);
+        // console.log("second card detected! This card is");
+        // console.log(secondCard);
         checkMatching()
     }
 }
