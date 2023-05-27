a7b372995cc539cef170a912dc92193b2a623cd3
lwu1822 <lwu1822@gmail.com>
2023-05-27 15:47:09 -0700

    settings tab only shows in navbar if user logged in

--- cookieCheck.js
+++ cookieCheck.js
@@ -4,6 +4,7 @@ let cookie = document.cookie + ";";
 let correctCookie = cookie.match(/(session=.*; |; session=.*;$|; session=.*; )/);
 
 if (correctCookie === null) {
+    // show "Log In" in navbar
     let a = document.createElement("a");
     let text = document.createTextNode("Log In");
     a.appendChild(text);
@@ -14,6 +15,7 @@ if (correctCookie === null) {
 
     document.getElementById("loginStatus").appendChild(li);
 } else {
+    // show "Log Out" in navbar
     let a = document.createElement("a");
     let text = document.createTextNode("Log Out");
     a.appendChild(text);
@@ -23,5 +25,18 @@ if (correctCookie === null) {
     li.appendChild(a);
 
     document.getElementById("loginStatus").appendChild(li);
+
+
+    // show "Settings" in navbar
+    a = document.createElement("a");
+    text = document.createTextNode("Settings");
+    a.appendChild(text);
+    a.href = window.location.origin + "/settings";
+
+    li = document.createElement("li");
+    li.appendChild(a);
+
+    document.getElementById("settings").appendChild(li);
+
 }
 
