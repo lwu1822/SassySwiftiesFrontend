06609c8336b0c50914d8d6ba38d87e1007f2c1e9
lwu1822 <lwu1822@gmail.com>
2023-05-27 19:41:17 -0700

    async/await part 2

--- getUsername.js
+++ getUsername.js
@@ -1,63 +1,89 @@
 async function fetchUsername() {
-        // retrieve cookie's value
-        console.log(document.cookie);
+    // retrieve cookie's value
+    console.log(document.cookie);
 
-        let cookie2 = document.cookie + ";";
-        let correctCookie2 = cookie2.match(/(session=.*; |; session=.*;$|; session=.*; )/);
-        correctCookie2 = correctCookie2[0].replace(/;/g, '');
-        correctCookie2 = correctCookie2.replace('session=', '');
-        correctCookie2 = correctCookie2.replace(/ /g, '');
-        console.log(correctCookie2);
+    let cookie2 = document.cookie + ";";
+    let correctCookie2 = cookie2.match(/(session=.*; |; session=.*;$|; session=.*; )/);
+    correctCookie2 = correctCookie2[0].replace(/;/g, '');
+    correctCookie2 = correctCookie2.replace('session=', '');
+    correctCookie2 = correctCookie2.replace(/ /g, '');
+    console.log(correctCookie2);
 
-        // send fetch to backend's endpoint for user info
-        var baseurl = "https://taylorswifties.duckdns.org/api/users/info";
-        
-        const body = {
-            token: correctCookie2,
-        };
+    // send fetch to backend's endpoint for user info
+    var baseurl = "https://taylorswifties.duckdns.org/api/users/info";
+    
+    const body = {
+        token: correctCookie2,
+    };
 
-        // Set Headers to support cross origin
-        //IMPORTANT!!!!!!! TO SUCCESSFULLY POST, YOU NEED TO REMOVE
-        // credentials:'include'
-        const requestOptions = {
-            method: 'POST',
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            //credentials: 'include', // include, *same-origin, omit
-            body: JSON.stringify(body),
-            headers: {
-                "content-type": "application/json"
-            },
-        };
+    // Set Headers to support cross origin
+    //IMPORTANT!!!!!!! TO SUCCESSFULLY POST, YOU NEED TO REMOVE
+    // credentials:'include'
+    const requestOptions = {
+        method: 'POST',
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        //credentials: 'include', // include, *same-origin, omit
+        body: JSON.stringify(body),
+        headers: {
+            "content-type": "application/json"
+        },
+    };
 
-        fetch(baseurl, requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                const errorMsg = 'Login error: ' + response.status;
-                console.log(errorMsg);
+    let response = await fetch(baseurl, requestOptions)
+    let data = await response.json();
+    return data; 
 
-                if (response.status == 400) {
-                    console.log("Incorrect username or password");
-                }
-                return;
+    /*
+    .then(response => {
+        if (!response.ok) {
+            const errorMsg = 'Login error: ' + response.status;
+            console.log(errorMsg);
+
+            if (response.status == 400) {
+                console.log("Incorrect username or password");
             }
+            return;
+        }
 
-            response.json().then(data => {
-                console.log(data);
-                
-                // show username
-                let p = document.createElement("p");
-                p.setAttribute("class", "inline");
-                let text = document.createTextNode(data["sub"]);
-                p.appendChild(text);
-                document.getElementById("username").appendChild(p);
-                
-
-            })
+        response.json().then(data => {
+            console.log(data);
+           
+           
+            
+            // show username
+            let p = document.createElement("p");
+            p.setAttribute("class", "inline");
+            let text = document.createTextNode(data["sub"]);
+            p.appendChild(text);
+            document.getElementById("username").appendChild(p);
+            
+            
 
         })
-    }
+        
 
-    async function showUsername() {
-        await fetchUsername();
-    }
+    })
+    */
+}
+
+/*
+fetchUsername().then(data => {
+    console.log("data: " + JSON.stringify(data));
+});
+*/
+
+async function showUsername() {
+    let data = await fetchUsername();
+
+    // debugging
+    console.log("data: " + JSON.stringify(data));
+
+
+    let p = document.createElement("p");
+    p.setAttribute("class", "inline");
+    let text = document.createTextNode(data["sub"]);
+    p.appendChild(text);
+    document.getElementById("username").appendChild(p);
+}
+
