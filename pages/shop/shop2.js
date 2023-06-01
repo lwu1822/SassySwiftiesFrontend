f2772cc73523888f285656b0e9663c9db5c206ca
lwu1822 <lwu1822@gmail.com>
2023-06-01 14:53:53 -0700

    get username

--- pages/shop/shop2.js
+++ pages/shop/shop2.js
@@ -1,11 +1,71 @@
-$(document).ready( function() {
-    id = 13
-    createNfts(id)
+async function fetchUsername() {
+    // retrieve cookie's value
+    console.log(document.cookie);
+
+    let cookie2 = document.cookie + ";";
+    let correctCookie2 = cookie2.match(/(session=.*;|; session=.*;$|; session=.*; )/);
+    correctCookie2 = correctCookie2[0].replace(/;/g, '');
+    correctCookie2 = correctCookie2.replace('session=', '');
+    correctCookie2 = correctCookie2.replace(/ /g, '');
+    console.log(correctCookie2);
+
+    // send fetch to backend's endpoint for user info
+    var baseurl = "https://taylorswifties.duckdns.org/api/users/info";
+    
+    const body = {
+        token: correctCookie2,
+    };
+
+    // Set Headers to support cross origin
+    //IMPORTANT!!!!!!! TO SUCCESSFULLY POST, YOU NEED TO REMOVE
+    // credentials:'include'
+    const requestOptions = {
+        method: 'POST',
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        //credentials: 'include', // include, *same-origin, omit
+        body: JSON.stringify(body),
+        headers: {
+            "content-type": "application/json"
+        },
+    };
+
+    let response = await fetch(baseurl, requestOptions)
+
+    if (!response["ok"]) {
+        window.location.href = "https://lwu1822.github.io/SassySwiftiesFrontend/logout";
+    }
+
+    let data = await response.json();
+    return data; 
+
+}
+
+
+
+async function fetchUsernameOnly() {
+    let data = await fetchUsername();
+    return data["sub"];
+}
+
+
+
+
+$(document).ready(function() {
+    //console.log("username: " + await fetchUsernameOnly());
+    start();
     //updatePage({"id":1,"nfts":[true,true,false,false,false,false],"profile":0,"userID":13})
 });
 
+async function start() {
+    console.log("username: " + await fetchUsernameOnly());
+
+    id = 13
+    //createNfts(id)
+}
+
 function createNfts(id) {
-    var url = "https://taylorswifties.duckdns.org/api/nfts/post";
+    var url = "https://taylorswifties.duckdns.org/api/nfts/create";
     fetch(url, {
         method: "POST",
         headers: {
