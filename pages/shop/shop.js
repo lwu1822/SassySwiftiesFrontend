b5fd6ee96aa26f80ecc2d0b766011b134f7d57c2
BobTheFarmer <colinmweis@gmail.com>
2023-06-01 20:39:15 -0700

    url fix

--- pages/shop/shop.js
+++ pages/shop/shop.js
@@ -13,7 +13,7 @@ $(document).ready(async function() {
         // Create a new container for the profile
         let container = $('<div>').addClass('container');
         // Add the profile image
-        container.append($('<img>').addClass('img').attr('src', '/images/profiles/profile_' + i + '.jpeg'));
+        container.append($('<img>').addClass('img').attr('src', '/SassySwiftiesFrontend/images/profiles/profile_' + i + '.jpeg'));
         // Add the status text
         if (nfts[i]) {
             container.append($('<h4>').text('You Own This!'));
